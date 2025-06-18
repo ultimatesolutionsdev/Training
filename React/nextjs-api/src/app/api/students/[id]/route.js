bf117ab56dfbcf4e34769f5d9cf933b5aeedd4a9
ultimatesolutionsdev <ultimatesolutionsdev@gmail.com>
2025-06-18 15:21:20 +0630

    Validation

--- React/nextjs-api/src/app/api/students/[id]/route.js
+++ React/nextjs-api/src/app/api/students/[id]/route.js
@@ -1,11 +1,69 @@
 import { NextResponse } from "next/server";
+import * as yup from "yup";
 
+//Validation Schema to validate client request.
+const schema = yup.object().shape({
+  name: yup.string().required("Name is required"),
+  fatherName: yup.string().required("Father Name is required"),
+  address: yup.string().required("Address is required"),
+  age: yup.number().required("Age is required"),
+  major: yup.string().required("Major is required"),
+});
+
+// Update Student API
 export async function PUT(req, { params }) {
+  try {
+    const studentId = params.id; // get URI params field;
+    const body = await req.json();
+    await schema.validate(body, { abortEarly: false }); // Call validation Schema
+    return NextResponse.json({
+      message: "Student is successfully updated.",
+      studentId,
+      bodyData: body,
+    });
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      return NextResponse.json(
+        {
+          message: "Validation failed",
+          errors: error.inner.map((e) => ({
+            path: e.path,
+            message: e.message,
+          })),
+        },
+        { status: 400 }
+      );
+    }
+    return NextResponse.json(
+      {
+        message: "Unexpected error",
+        error: error.message,
+      },
+      { status: 500 }
+    );
+  }
+}
+
+// Delete Student API
+export async function DELETE(req, { params }) {
   const studentId = params.id; // get URI params field;
-  const body = await req.json();
   return NextResponse.json({
-    message: "Student is successfully updated.",
+    message: "Student is successfully deleted.",
     studentId,
-    bodyData: body,
   });
+}
+
+// Get Student Detail API
+export async function GET(req, { params }) {
+  const studentId = params.id; // get URI params field;
+  const student = {
+    id: studentId,
+    name: "Su Su",
+    age: 18,
+    gender: "female",
+    fatherName: "U Maung",
+    address: "Hledan",
+    major: "Computer Science",
+  };
+  return NextResponse.json(student);
 }
--- React/nextjs-api/src/app/api/students/route.js
+++ React/nextjs-api/src/app/api/students/route.js
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import * as yup from "yup";
 
 const StudentData = [
   {
@@ -23,14 +24,50 @@ const StudentData = [
     major: "Computer Science",
   },
 ];
+
+// Get Student List API
 export async function GET() {
   return NextResponse.json(StudentData);
 }
 
+//Validation Schema to validate client request.
+const schema = yup.object().shape({
+  name: yup.string().required("Name is required"),
+  fatherName: yup.string().required("Father Name is required"),
+  address: yup.string().required("Address is required"),
+  age: yup.number().required("Age is required"),
+  major: yup.string().required("Major is required"),
+});
+
+// Create Student API
 export async function POST(req) {
-  const body = await req.json();//Get requested body data from client
-  return NextResponse.json({
-    message: "Student is successfully created.",
-    bodyData: body,
-  });
+  try {
+    const body = await req.json(); //Get requested body data from client
+    await schema.validate(body, { abortEarly: false }); // Call validation Schema
+    return NextResponse.json({
+      message: "Student is successfully created.",
+      bodyData: body,
+    });
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      return NextResponse.json(
+        {
+          message: "Validation failed",
+          errors: error.inner.map((e) => ({
+            path: e.path,
+            message: e.message,
+          })),
+        },
+        { status: 400 }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        message: "Unexpected error",
+        error: error.message,
+      },
+      { status: 500 }
+    );
+  }
 }
